5915d7b4e292c1e2fd5622e2236f135287bd4110
Clint Taylor <stephen.c.taylor72@gmail.com>
2016-10-03 13:38:37 -0500

    Added questions and point values for all controller and partials for questionSet1 using the model that Jose provided.

--- quiz/questionSet1/questionsPage2/1-2.ctrl.js
+++ quiz/questionSet1/questionsPage2/1-2.ctrl.js
@@ -1,6 +1,66 @@
 angular.module('app')
-  .controller('OneTwoController', OneTwoController)
+.controller('OneTwoController', OneTwoController)
 
-function OneTwoController() {
-  
+OneTwoController.$inject = ['$scope', 'CheckService'];
+
+function OneTwoController($scope, CheckService) {
+
+var vm = this;
+
+vm.cb4 = CheckService.cb4; //Reads the status of the checkbox from service if this one has been assigned values, is empty by default
+vm.cb5 = CheckService.cb5;
+vm.cb6 = CheckService.cb6;
+vm.cbs1q21 = CheckService.cbs1q21;
+vm.cbs1q22 = CheckService.cbs1q22;
+vm.cbs1q23 = CheckService.cbs1q23;
+vm.cbs1q24 = CheckService.cbs1q24;
+vm.cbs1q25 = CheckService.cbs1q25;
+
+vm.currentPage = CheckService.page2; //Reads the points achieved by this particular page from the service, by default is 0.
+
+vm.total = CheckService.total; //Reads the points achieved in all the pages, by default is 0.
+
+$scope.cbChange = function(val, selectedCb) { //Function that takes the numerical number associated with the checkbox and the name of the checkbox to save the value to the corresponding object in the service (cb4 to cb6 in this page)
+
+switch(selectedCb)
+{
+        
+case 'cbs1q21':
+
+CheckService.cbs1q21 = val;
+
+break;
+
+case 'cbs1q22':
+
+CheckService.cbs1q22 = val;
+
+break;
+
+case 'cbs1q23':
+
+CheckService.cbs1q23 = val;
+
+break;
+        
+case 'cbs1q24':
+
+CheckService.cbs1q24 = val;
+
+break;
+
+case 'cbs1q25':
+
+CheckService.cbs1q25 = val;
+
+break;
+}
+
+CheckService.page2+= val; //updates the total for the page 1 into the service
+vm.currentPage = CheckService.page2; //Updates the current view (text box)
+
+CheckService.total = (CheckService.page1 + CheckService.page2 + CheckService.page3); //Adds all the points achieved in all pages
+vm.total = CheckService.total; //Updates the view (text box)
+
+}
 }
--- quiz/questionSet1/questionsPage4/1-4.ctrl.js
+++ quiz/questionSet1/questionsPage4/1-4.ctrl.js
@@ -1,6 +1,68 @@
 angular.module('app')
-  .controller('OneFourController', OneFourController)
+.controller('OneFourController', OneFourController)
 
-function OneFourController() {
-  
+OneFourController.$inject = ['$scope', 'CheckService']
+
+function OneFourController($scope, CheckService) {
+
+var vm = this;
+
+vm.cb1 = CheckService.cb1; //Reads the status of the checkbox from service if this one has been assigned values, is empty by default
+vm.cb2 = CheckService.cb2;
+vm.cb3 = CheckService.cb3;
+    
+//cbs1q11 == cb set 1 page 1 question 1
+//cbs1q12 == cb set 1 page 1 question 2
+vm.cbs1q41 = CheckService.cbs1q41;
+vm.cbs1q42 = CheckService.cbs1q42;
+vm.cbs1q43 = CheckService.cbs1q43;
+vm.cbs1q44 = CheckService.cbs1q44;
+vm.cbs1q45 = CheckService.cbs1q45;
+
+vm.currentPage = CheckService.page1; //Reads the points achieved by this particular page from the service, by default is 0.
+
+vm.total = CheckService.total; //Reads the points achieved in all the pages, by default is 0.
+
+$scope.cbChange = function(val, selectedCb) { //Function that takes the numerical number associated with the checkbox and the name of the checkbox to save the value to the corresponding object in the service (cb1 to cb3 in this page)
+
+switch(selectedCb)
+{
+case 'cbs1q41':
+
+CheckService.cbs1q41 = val;
+
+break;
+
+case 'cbs1q42':
+
+CheckService.cbs1q42 = val;
+
+break;
+
+case 'cbs1q43':
+
+CheckService.cbs1q43 = val;
+
+break;
+        
+case 'cbs1q44':
+
+CheckService.cbs1q44 = val;
+
+break;
+        
+case 'cbs1q45':
+
+CheckService.cbs1q45 = val;
+
+break;
+}
+
+CheckService.page1+= val; //updates the total for the page 1 into the service
+vm.currentPage = CheckService.page1; //Updates the current view (text box)
+
+CheckService.total = (CheckService.page1 + CheckService.page2 + CheckService.page3); //Adds all the points achieved in all pages
+vm.total = CheckService.total; //Updates the view (text box)
+
+}
 }
