523b49b0710259ab10aa79fa6722178c92859748
Kelly Douglass <kdouglass@gmail.com>
2016-11-05 00:01:54 -0500

    updated test controller

---
+++ testLookup/test.service.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('CityService', CityService);
+
+    CityService.$inject = ['$http'];
+    function CityService($http) {
+        var service = {city: null}
+        service.city = cityList()
+        
+        return service;
+
+        ////////////////
+        function cityList() {
+            $http.get('../data/city_county.json').success(function (data) {
+                service.city = data
+                
+            });
+        }
+    }
+})();
--- testLookup/testCtrl.js
+++ testLookup/testCtrl.js
@@ -1,12 +1,13 @@
 angular.module('app')
     .controller('TestController', TestController)
 
-TestController.$inject = ['$scope']
+TestController.$inject = ['$scope', 'CityService']
 
-function TestController($scope) {
-    vm = this
+function TestController($scope, CityService) {
+    vm = this;
 
-  
+   $scope.citiesList = CityService
+   
  
     
     //changes input to lowercase if vm.test has anything input 
@@ -14,3 +15,16 @@ function TestController($scope) {
         vm.test.toLowerCase()
     }
 }
+
+
+/*theApp.factory('mainInfo', function($http) { 
+
+    var obj = {content:null};
+
+    $http.get('content.json').success(function(data) {
+        // you can do some processing here
+        obj.content = data;
+    });    
+
+    return obj;    
+});*/
