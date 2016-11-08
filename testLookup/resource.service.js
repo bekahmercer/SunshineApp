b0bfb5d4be2fa6e7d367d18c00c15e7db8d99bb4
Kelly Douglass <kdouglass@gmail.com>
2016-11-08 17:40:31 -0600

    ''

---
+++ testLookup/resource.service.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('ResourceService', ResourceService);
+
+    ResourceService.$inject = ['$http'];
+    function ResourceService($http) {
+        var service = {resources: null}
+        service.resources = resourceList()
+        return service
+
+        ////////////////
+        function resourceList() {
+            $http.get('../data/resources_temp.json').success(function (data) {
+               service.resources = data
+               
+               
+            });
+        }
+    }
+})();
--- testLookup/testCtrl.js
+++ testLookup/testCtrl.js
@@ -1,14 +1,25 @@
 angular.module('app')
     .controller('TestController', TestController)
 
-TestController.$inject = ['$scope', 'CityService']
+TestController.$inject = ['$scope', 'CityService', 'ResourceService']
 
-function TestController($scope, CityService) {
+function TestController($scope, CityService, ResourceService) {
     vm = this;
 
    $scope.citiesList = CityService
    
+  $scope.resourceArray = ResourceService
+  console.log($scope.resourceArray)
  
+
+function matchCounty(countyFromCounties) {
+    $scope.resourceArray.resources.forEach(function (countyFromCounties) {
+        if (countyFromCounties == resourceArray.county) {
+            console.log("hello")
+        }
+    })
+}
+
     
     //changes input to lowercase if vm.test has anything input 
     if (vm.test) {
