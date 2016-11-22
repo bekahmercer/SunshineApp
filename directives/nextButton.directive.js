b69bb301db0c0041a063b2046c1fed3009d6536d
Kelly Douglass <kdouglass@gmail.com>
2016-11-22 17:12:11 -0600

    moved next button below body div

--- directives/nextButton.directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-    .directive('nextButton', nextButton)
-
-    function nextButton() {
-        return {
-            templateUrl: '/directives/nextButton.html'
-        }
-    }
