3b81c58d2b23efabe1d93b9aa296d7db323f9dc6
Jose Roberto Orellana Jr <yoborellana_2004@yahoo.com>
2016-11-22 11:06:51 -0600

    added progress bar to summary one

--- quiz/questionSet1/summary1/summary1.ctrl.js
+++ quiz/questionSet1/summary1/summary1.ctrl.js
@@ -9,4 +9,20 @@ vm = this;
 CheckService.qs1summary = (CheckService.qs1p1 + CheckService.qs1p2 + CheckService.qs1p3 + CheckService.qs1p4);
 vm.total = CheckService.qs1summary;
 
+
+if(vm.total>=0 && vm.total<=9)
+{
+vm.scale = "success";
+vm.riskm = "You are in low risk";
+}
+else if (vm.total>=10 & vm.total<=19)
+{
+vm.scale = "warning";
+vm.riskm = "Your risk is moderate";
+}
+else {
+vm.scale = "danger";
+vm.riskm = "You really are in high risk";
+}
+
 }
