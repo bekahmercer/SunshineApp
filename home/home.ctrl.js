fefd6cb0677a8f8b8b895710f96ce3cd1dbea33a
Clint Taylor <stephen.c.taylor72@gmail.com>
2016-12-02 00:09:10 -0600

    created a mock website: files edited include main.css, home.partial, home.ctrl, index.html, and activities.json.  The activities file randomly picks three events from the list and displays them.  Added a leaf trail to get to main page.

--- home/home.ctrl.js
+++ home/home.ctrl.js
@@ -1,6 +1,10 @@
-angular.module('app')
-  .controller('HomeController', HomeController)
+angular.module('app').controller('HomeController', HomeController)
 
-function HomeController() {
-  
-}
+function HomeController($scope, $http) {
+    $http.get('data/activities.json').then(function (res) {
+        $scope.activities = res.data;
+        $scope.random = function () {
+            return 0.5 - Math.random();
+        };
+    });
+};
