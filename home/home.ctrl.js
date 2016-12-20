498d24af3c17c233a23a2c4badf9b3d84b16855b
Clint Taylor <stephen.c.taylor72@gmail.com>
2016-12-20 17:52:39 -0600

    fixed error in console on home html page, event list no longer randomizes, but takes first 3 of the event array

--- home/home.ctrl.js
+++ home/home.ctrl.js
@@ -3,8 +3,5 @@ angular.module('app').controller('HomeController', HomeController)
 function HomeController($scope, $http) {
     $http.get('data/activities.json').then(function (res) {
         $scope.activities = res.data;
-        $scope.random = function () {
-            return 0.5 - Math.random();
-        };
     });
 };
