add66fcf941ed390bd8a6d3812956d0601d80755
Daniel <danielkline19@hotmail.com>
2017-01-04 13:20:26 -0600

    progress bar text summary changes color

--- quiz/questionSet1/summary1/summary1.ctrl.js
+++ quiz/questionSet1/summary1/summary1.ctrl.js
@@ -5,35 +5,37 @@ SummaryOneController.$inject = ['$scope', 'CheckService']
 
 function SummaryOneController($scope, CheckService) {
 
-vm = this;
-
-vm.scrollUp = function() {
-    window.scrollTo(0, 0);
-  }
-
-CheckService.qs1summary = (CheckService.qs1p1 + CheckService.qs1p2 + CheckService.qs1p3 + CheckService.qs1p4);
-vm.total = CheckService.qs1summary;
-
-$scope.getPercentage = function(maxVal)
-{
-vm.pbarData = CheckService.calculatePercentage(maxVal, vm.total);
-
-return vm.pbarData
-}
-
-if(vm.total>=0 && vm.total<=9)
-{
-vm.scale = "success";
-vm.riskm = "You don't have many relationship risk factors.";
-}
-else if (vm.total>=10 & vm.total<=19)
-{
-vm.scale = "warning";
-vm.riskm = "You have a some risk factors to consider.";
-}
-else {
-vm.scale = "danger";
-vm.riskm = "You have several risk factors to consider.";
-}
-
+    vm = this;
+    
+    vm.scrollUp = function() {
+        window.scrollTo(0, 0);
+    }
+    
+    CheckService.qs1summary = (CheckService.qs1p1 + CheckService.qs1p2 + CheckService.qs1p3 + CheckService.qs1p4);
+    vm.total = CheckService.qs1summary;
+    
+    $scope.getPercentage = function(maxVal)
+    {
+        vm.pbarData = CheckService.calculatePercentage(maxVal, vm.total);
+        return vm.pbarData
+    }
+    
+    if(vm.total>=0 && vm.total<=9)
+        {
+        vm.scale = "success";
+        vm.riskm = "You don't have many relationship risk factors.";
+        // shade of green
+        vm.color = "#5cb85c";
+        }
+        else if (vm.total>=10 & vm.total<=19)
+        {
+        vm.scale = "warning";
+        vm.riskm = "You have some risk factors to consider.";
+        vm.color = "orange";
+        }
+        else {
+        vm.scale = "danger";
+        vm.riskm = "You have several risk factors to consider.";
+        vm.color = "red";
+        }
 }
--- quiz/questionSet2/summary2/summary2.ctrl.js
+++ quiz/questionSet2/summary2/summary2.ctrl.js
@@ -6,40 +6,39 @@ SummaryTwoController.$inject = ['$scope', 'CheckService'];
 function SummaryTwoController ($scope, CheckService) {
     vm = this;
 
-CheckService.qs2summary = (CheckService.qs2p1 + CheckService.qs2p2 + CheckService.qs2p3 + CheckService.qs2p4 + CheckService.qs2p5);
-vm.total = CheckService.qs2summary;
-
-vm.p1 = CheckService.qs2p1;
-vm.p2 = CheckService.qs2p2;
-vm.p3 = CheckService.qs2p3;
-vm.p4 = CheckService.qs2p4;
-vm.p5 = CheckService.qs2p5;
-
-$scope.getPercentage = function(maxVal)
-{
-vm.pbarData = CheckService.calculatePercentage(maxVal, vm.total);
- 
-return vm.pbarData
-}
-
-
-
-if((vm.p1>=0 && vm.p1<=2) && (vm.p2>=0 && vm.p2<=2) && (vm.p3>=0 && vm.p3<=2) && (vm.p4>=0 && vm.p4<=2) && (vm.p5>=0 && vm.p5<=2))
-  {
- vm.scale = "success";
- vm.riskm = "You don't have many relationships factors";
- }
- if ((vm.p1>=3 && vm.p1<=4) || (vm.p2>=3 && vm.p2<=4) || (vm.p3>=3 && vm.p3<=4) || (vm.p4>=3 && vm.p4<=4) || (vm.p5>=3 && vm.p5<=4))
- {
- vm.scale = "warning";
- vm.riskm = "You have a some risk factors to consider";
- }
-  
- if ((vm.p1>=5) || (vm.p2>=5) || (vm.p3>=5) || (vm.p4>=5) || (vm.p5>=5))
- {
- vm.scale = "danger";
- vm.riskm = "You have several risk factors to consider";
- }
-
+    CheckService.qs2summary = (CheckService.qs2p1 + CheckService.qs2p2 + CheckService.qs2p3 + CheckService.qs2p4 + CheckService.qs2p5);
+    vm.total = CheckService.qs2summary;
+    
+    vm.p1 = CheckService.qs2p1;
+    vm.p2 = CheckService.qs2p2;
+    vm.p3 = CheckService.qs2p3;
+    vm.p4 = CheckService.qs2p4;
+    vm.p5 = CheckService.qs2p5;
+    
+    $scope.getPercentage = function(maxVal)
+    {
+        vm.pbarData = CheckService.calculatePercentage(maxVal, vm.total);
+        return vm.pbarData
+    }
+    
+    if((vm.p1>=0 && vm.p1<=2) && (vm.p2>=0 && vm.p2<=2) && (vm.p3>=0 && vm.p3<=2) && (vm.p4>=0 && vm.p4<=2) && (vm.p5>=0 && vm.p5<=2))
+    {
+        vm.scale = "success";
+        vm.riskm = "You don't have many relationships factors";
+        // shade of green
+        vm.color = "#5cb85c";
+    }
+    if ((vm.p1>=3 && vm.p1<=4) || (vm.p2>=3 && vm.p2<=4) || (vm.p3>=3 && vm.p3<=4) || (vm.p4>=3 && vm.p4<=4) || (vm.p5>=3 && vm.p5<=4))
+    {
+        vm.scale = "warning";
+        vm.riskm = "You have a some risk factors to consider";
+        vm.color = "orange";
+    }
+    if ((vm.p1>=5) || (vm.p2>=5) || (vm.p3>=5) || (vm.p4>=5) || (vm.p5>=5))
+    {
+        vm.scale = "danger";
+        vm.riskm = "You have several risk factors to consider";
+        vm.color = "red";
+    }
 
 }
