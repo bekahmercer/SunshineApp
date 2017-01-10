584368d76b05f5691c5d988264b154d1b30dd687
Clint Taylor <stephen.c.taylor72@gmail.com>
2017-01-10 17:35:12 -0600

    add parks.json file, style parks section in main.css

--- home/home.ctrl.js
+++ home/home.ctrl.js
@@ -3,5 +3,9 @@ angular.module('app').controller('HomeController', HomeController)
 function HomeController($scope, $http) {
     $http.get('data/activities.json').then(function (res) {
         $scope.activities = res.data;
+        return $http.get('data/parks.json');
+    }).then(function(res2) {
+        $scope.parks = res2.data; // get data from json
+    
     });
 };
